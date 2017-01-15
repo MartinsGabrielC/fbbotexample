7fc2e853f1889078b90d0bf69469bfee23bb45b3
Gabriel <martins.gabriel.c@gmail.com>
2017-01-15 20:39:09 -0200

    Created Echo Bot Fixed

--- index.js
+++ index.js
@@ -22,32 +22,32 @@ app.get('/webhook', function(req, res){
 });
 
 //Handling receiving messages
-app.post('/webhook',function(req, res){
+app.post('/webhook', function (req, res) {
   var events = req.body.entry[0].messaging;
-  for (var i = 0; i < events.length; i++) {
-    var event1 = events[i];
-    if (event1.message && event1.message.text){
-      sendMessage(event1.sender.id, {text: "Echo: " + event1.message.text});
-    }
+  for (i = 0; i < events.length; i++) {
+      var event = events[i];
+      if (event.message && event.message.text) {
+          sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+      }
   }
   res.sendStatus(200);
 });
 
 //Generic message sending function
-function sendMessage(recipientId, message){
+function sendMessage(recipientId, message) {
   request({
-    url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
-    method: 'POST',
-    json: {
-      recipient: {id: recipientId},
-      message: message,
-    }, function(error, response, body){
-      if (error){
-        console.log('Error sending message: ', error);
-      }else if (response.body.error){
-        console.log('Error: ', response.body.error);
+      url: 'https://graph.facebook.com/v2.6/me/messages',
+      qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+      method: 'POST',
+      json: {
+          recipient: {id: recipientId},
+          message: message,
       }
-    }
-  });
+    }, function(error, response, body) {
+      if (error) {
+          console.log('Error sending message: ', error);
+      } else if (response.body.error) {
+          console.log('Error: ', response.body.error);
+      }
+    });
 };
