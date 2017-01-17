769e5f708376578cf7025793e9e284096d0fdd67
Gabriel Martins <martins.gabriel.c@gmail.com>
2017-01-17 16:59:33 -0200

    Added media files

--- index.js
+++ index.js
@@ -389,11 +389,11 @@ function receivedMessage(event){
   var recipientID = event.recipient.id;
   var timeOfMessage = event.timestamp;
   var message = event.message;
-
-  console.log("Received message from user %d for page %d at %d with message: ", senderID,recipientID,timeOfMessage);
-  console.log(JSON.stringify(message));
-
   var messageText = message.text;
+
+  console.log("Received message from user %d for page %d at %d with message: %s", senderID,recipientID,timeOfMessage,messageText);
+
+
   var messageAttachments = message.attachments;
   if(messageText){
     switch (messageText) {
