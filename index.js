d56a19a192f60dd2b45423e4731ab2cf2ea5754a
Gabriel Martins <martins.gabriel.c@gmail.com>
2017-01-17 10:56:52 -0200

    Minor changes, added log for message delivery

--- index.js
+++ index.js
@@ -1,42 +1,64 @@
 const
   bodyParser = require('body-parser'),
-  config = require('config'),
+  //config = require('config'),
   express = require('express'),
   https = require('https'),
   request = require('request');
 var app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+app.use(bodyParser.json({verify: verifyRequestSignature}));
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function(){
   console.log('Node app is runing on port', app.get('port'));
 });
 
 // App Secret can be retrieved from the App Dashboard
-const APP_SECRET = (process.env.MESSENGER_APP_SECRET) ?
-  process.env.MESSENGER_APP_SECRET :
-  config.get('appSecret');
+const APP_SECRET = process.env.MESSENGER_APP_SECRET;
 
 // Arbitrary value used to validate a webhook
-const VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN) ?
-  (process.env.MESSENGER_VALIDATION_TOKEN) :
-  config.get('validationToken');
+const VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN);
 
 // Generate a page access token for your page from the App Dashboard
-const PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
-  (process.env.MESSENGER_PAGE_ACCESS_TOKEN) :
-  config.get('pageAccessToken');
+const PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN);
 
 // URL where the app is running (include protocol). Used to point to scripts and
 // assets located at this address.
-const SERVER_URL = (process.env.SERVER_URL) ?
-  (process.env.SERVER_URL) :
-  config.get('serverURL');
+const SERVER_URL = (process.env.SERVER_URL);
 
 if (!(APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN && SERVER_URL)) {
   console.error("Missing config values");
   process.exit(1);
+}
+
+/*
+ * Verify that the callback came from Facebook. Using the App Secret from
+ * the App Dashboard, we can verify the signature that is sent with each
+ * callback in the x-hub-signature field, located in the header.
+ *
+ * https://developers.facebook.com/docs/graph-api/webhooks#setup
+ *
+ */
+function verifyRequestSignature(req, res, buf) {
+  var signature = req.headers["x-hub-signature"];
+
+  if (!signature) {
+    // For testing, let's log an error. In production, you should throw an
+    // error.
+    console.error("Couldn't validate the signature.");
+  } else {
+    var elements = signature.split('=');
+    var method = elements[0];
+    var signatureHash = elements[1];
+
+    var expectedHash = crypto.createHmac('sha1', APP_SECRET)
+                        .update(buf)
+                        .digest('hex');
+
+    if (signatureHash != expectedHash) {
+      throw new Error("Couldn't validate the request signature.");
+    }
+  }
 }
 
 //Server Frontpage
@@ -46,7 +68,7 @@ app.get('/',function(req, res){
 
 //Facebook Webhook
 app.get('/webhook', function(req, res){
-  if(req.query['hub.verify_token'] === VALIDATION_TOKEN){
+  if(req.query['hub.mode'] === "subscribe" && req.query['hub.verify_token'] === VALIDATION_TOKEN){
     res.status(200).send(req.query['hub.challenge']);
   }else{
     console.error("Failed validation. Make sure the validation tokens match.");
@@ -406,7 +428,8 @@ function receivedPostback(event) {
   // When a postback is called, we'll send a message back to the sender to
   // let them know it was successful
   if (payload === "GET_STARTED"){
-    var tutorial = "Message: \n"
+    var tutorial = "Welcome to the bot example. Let's get started!\n"
+    +"Message: \n"
     + "\"img\" to receive an image message\n"
     +"\"gif\" to receive a gif message\n"
     +"\"audio\" to receive an audio message\n"
@@ -416,7 +439,6 @@ function receivedPostback(event) {
     +"\"generic\" to receive a generic template\n"
     +"\"list\" to receive a list template\n"
     +"anything else will be echo\'ed"
-    sendTextMessage(senderID,"Welcome to the bot example. Let's get started!");
     sendTextMessage(senderID,tutorial);
   }else if (payload === "LETS_CHAT"){
     sendTextMessage(senderID,"Sure, what do you want to do next?");
